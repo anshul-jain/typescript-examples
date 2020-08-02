8638ff5c803fadaa6245ec69321571287434cd6f
anshul.jain <anshul.jain@appdirect.com>
2020-08-02 19:01:37 +0530

    Number example.

--- core-type/app.ts
+++ core-type/app.ts
@@ -1,1 +1,8 @@
-console.log('Your code goes here...');
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+const number1 = `10`; // Editor and compiler reports its error to fix, but it doesn't stop you to create the js file.
+const number2 = 20.2;
+
+const result = add(number1, number2);
+console.log(result);
