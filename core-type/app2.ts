88ce61deb5c4a0606480bd9c64b546d711dd4705
anshul.jain <anshul.jain@appdirect.com>
2020-08-08 19:02:28 +0530

    Object declaration.

---
+++ core-type/app2.ts
@@ -0,0 +1,21 @@
+let employee = {
+    name: 'ram',
+    age: 20
+};
+
+console.log(employee.name);
+
+// Type 2, you can explicitly define the object.
+let employee1: object = {
+    name: 'ram',
+    age: 20
+}
+
+// Type 3, define the structure by your self
+let employee2: {
+    name: string,
+    age: number
+} = {
+    name: 'ram',
+    age: 20
+}
