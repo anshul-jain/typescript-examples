7d8655b1e708d37c36e66ee30c20e4cb25b661c8
anshul.jain <anshul.jain@appdirect.com>
2020-08-09 11:18:16 +0530

    const

--- core-type/app2.ts
+++ core-type/app2.ts
@@ -1,4 +1,4 @@
-let employee = {
+const employee = {
     name: 'ram',
     age: 20
 };
@@ -6,15 +6,15 @@ let employee = {
 console.log(employee.name);
 
 // Type 2, you can explicitly define the object.
-let employee1: object = {
+const employee1: object = {
     name: 'ram',
     age: 20
 }
 
 // Type 3, define the structure by your self
 let employee2: {
-    name: string,
-    age: number
+    name: string;
+    age: number;
 } = {
     name: 'ram',
     age: 20
